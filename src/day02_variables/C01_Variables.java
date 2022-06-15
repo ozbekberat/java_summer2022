e0c68607beac201c8fe86233cfc5fc654bdc90ed
Berat <beraterdem309@gmail.com>
2022-06-15 12:15:31 +0300

    first commit

---
+++ src/day02_variables/C01_Variables.java
@@ -0,0 +1,27 @@
+package day02_variables;
+
+public class C01_Variables {
+    public static void main(String[] args) {
+
+            int             not                 =                60                     ;
+        // Data turu    variable ismi     assignment sign       variable degeri
+
+            int not2    =       70;
+        //  variable           deger
+        // Java once degeri hesaplar sonra assign islemini yapar
+
+        // not2'yi 80 yapalim
+        not2 = 80;
+
+
+        // yeni bir int variable olusturalim degeri ilk iki variable'im ortalamasi olsun
+
+        int ort = (not + not2) /2;
+
+        System.out.println(ort);
+        // " " icinde yazilan hersey metindir, Java " " icinde ne gorurse o sekilde yazdirir
+        // eger bir variable'in degerini yazdirmak istiyorsaniz
+        // " " olmadan variable ismini yazmalisiniz
+
+    }
+}
